e3f4007007f8bf5977b37f8a2e5b494a33ff0ca8
Minhthun744 <thuongminh744@gmail.com>
2025-01-04 13:06:01 +0700

    update

---
+++ tieuthiminhthuong/Controllers/ListController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace tieuthiminhthuong.Controllers
+{
+    public class ListController : Controller
+    {
+        // GET: List
+        public ActionResult ListingGrid()
+        {
+            return View();
+        }
+    }
+}
